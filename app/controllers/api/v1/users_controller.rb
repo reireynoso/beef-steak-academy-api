faf61d9ba4cd5421d88d946854610ee1388b6aac
Brenden Williams <brendenw7@gmail.com>
2019-06-18 09:33:31 -0400

    User show json route

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,28 @@ class Api::V1::UsersController < ApplicationController
     @users = User.all
     render json: @users
   end
+
+  def show
+    @user = find_user
+    render json: @user, status: :accepted
+  end
+
+  def update
+    @user.update(user_params)
+    if @user.save
+      render json: @user, status: :accepted
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+ 
+  private
+ 
+  def user_params
+    params.permit(:title,:description,:img_url,:subject)
+  end
+ 
+  def find_user
+    @user = User.find(params[:id])
+  end
 end
