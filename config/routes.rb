e71f35233120fdbf677b528ff8c398c70da834f6
Brenden Williams <brendenw7@gmail.com>
2019-06-18 11:08:33 -0400

    Testing Authorization Routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :courses, only: [:index, :update,:show]
       resources :users, only: [:index,:show,:create,:update,:destroy]
+      post "/login", to: "auth#login"
+
+			get "/auto_login", to: "auth#auto_login"
       resources :lists, only: [:index,:show, :create, :destroy]
       resources :tasks, only: [:index,:show, :create,:update,:destroy]
     end
