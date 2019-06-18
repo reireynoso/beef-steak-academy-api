b08aec57db350bf7c0bdfed552cdc6548c90ecd0
Brenden Williams <brendenw7@gmail.com>
2019-06-18 11:49:10 -0400

    Bruh

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -33,7 +33,7 @@ class Api::V1::UsersController < ApplicationController
   private
  
   def user_params
-    params.permit(:first_name,:last_name,:bio,:email,:password,:img_url)
+    params.require(:user).permit(:first_name,:last_name,:bio,:email,:password,:img_url)
   end
  
   def find_user
